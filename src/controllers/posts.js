f0429eef7cd3677528b19a0efb23bdd460a141b5
Ibrahim Kundlik <ibrahimkundlik@gmail.com>
2021-05-26 15:54:05 +0530

    Added POST request for creating posts

--- src/controllers/posts.js
+++ src/controllers/posts.js
@@ -10,12 +10,13 @@ export const getPosts = async (req, res) => {
 };
 
 export const createPosts = async (req, res) => {
-	const post = req.body;
-	const newPost = new PostMessage(post);
+	const { title, message, creator, tags } = req.body;
+
+	const newPost = new PostMessage({ title, message, creator, tags });
 
 	try {
 		await newPost.save();
-		res.status(201).send(newPost);
+		res.status(201).json(newPost);
 	} catch (error) {
 		res.status(409).json({ error: error.message });
 	}
