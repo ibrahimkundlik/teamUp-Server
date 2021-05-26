8be518baa9ae12eef9d77823b443674238d09a76
Ibrahim Kundlik <ibrahimkundlik@gmail.com>
2021-05-26 12:48:41 +0530

    Worked on dummy api build for posts

---
+++ src/controllers/posts.js
@@ -0,0 +1,22 @@
+import PostMessage from "../models/postMessage.js";
+
+export const getPosts = async (req, res) => {
+	try {
+		const postMessages = await PostMessage.find();
+		res.status(200).json(postMessages);
+	} catch (error) {
+		res.status(404).json({ error: error.message });
+	}
+};
+
+export const createPosts = async (req, res) => {
+	const post = req.body;
+	const newPost = new PostMessage(post);
+
+	try {
+		await newPost.save();
+		res.status(201).send(newPost);
+	} catch (error) {
+		res.status(409).json({ error: error.message });
+	}
+};
--- src/index.js
+++ src/index.js
@@ -1,13 +1,16 @@
 import express from "express";
-import bodyParser from "body-parser";
 import cors from "cors";
 import mongoose from "mongoose";
+import postRoutes from "./routes/posts.js";
 
 const app = express();
 
-app.use(bodyParser.json({ limit: "30mb", extended: true }));
-app.use(bodyParser.urlencoded({ limit: "30mb", extended: true }));
+app.use(express.json({ limit: "30mb", extended: true }));
+app.use(express.urlencoded({ limit: "30mb", extended: true }));
 app.use(cors());
+
+app.use("/posts", postRoutes);
+
 const PORT = process.env.PORT;
 
 mongoose
