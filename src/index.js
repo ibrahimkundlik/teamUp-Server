dfc0cbb27c80f28a1a768fce6be325d07bd72ede
Ibrahim Kundlik <ibrahimkundlik@gmail.com>
2021-06-03 08:44:00 +0530

    Removed post routes and controller

--- src/index.js
+++ src/index.js
@@ -1,7 +1,6 @@
 import express from "express";
 import cors from "cors";
 import mongoose from "mongoose";
-import postRoutes from "./routes/posts.js";
 import userRoutes from "./routes/user.routes.js";
 
 const app = express();
@@ -10,7 +9,6 @@ app.use(express.json({ limit: "30mb", extended: true }));
 app.use(express.urlencoded({ limit: "30mb", extended: true }));
 app.use(cors());
 
-app.use("/posts", postRoutes);
 app.use("/users", userRoutes);
 
 const PORT = process.env.PORT;
